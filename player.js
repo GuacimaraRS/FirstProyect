4d40a5e5c71204737c861847cc7cbce9612e72d2
Guacimara Ramirez Santana <guaci.rs10@gmail.com>
2023-07-19 11:53:32 +0100

    Created the moving player

--- player.js
+++ player.js
@@ -1,6 +1,10 @@
 function Player (x, y, parent){
+    let self = this;
     this.x = x;
     this.y = y;
+    this.directionX = 0;
+    this.directionY = 0;
+    this.speed = 10;
     this.sprite = document.createElement('div');
  
     this.createMosquito = function(){
@@ -8,6 +12,18 @@ function Player (x, y, parent){
         this.sprite.style.left = this.x + 'px'
         this.sprite.style.top = this.y + 'px'
         parent.appendChild(this.sprite)
+    }
+    this.move = function(){
+        let newX = self.x + self.speed * self.directionX
+        let newY = self.y + self.speed * self.directionY
+        if (newX >= 0 && newX <= 950){
+            self.x = newX
+            self.sprite.style.left = self.x + 'px'
+        }
+        if (newY >= 0 && newY <= 450){
+            self.y = newY
+            self.sprite.style.top = self.y + 'px'
+        }
     }
     
 }  
--- script.js
+++ script.js
@@ -3,12 +3,45 @@ import { Player } from "./player.js";
 const board = document.getElementById('board');
 let mosquito = new Player(0, 200, board);
 
+let playerMoveId;
+
 function start(){
     mosquito.createMosquito();
-  
+    playerMoveId = setInterval(playerMove, 50);
 }
 
 
+function playerMove() {
+    mosquito.move();
+}
+
+
+
+window.addEventListener('keydown', function(e) {
+    switch (e.key) {
+        case 'ArrowRight':
+            mosquito.directionX = 1;
+            break;
+        case 'ArrowLeft':
+            mosquito.directionX = -1;
+            break;
+        case 'ArrowUp':
+            mosquito.directionY = -1;
+            break;
+        case 'ArrowDown':
+            mosquito.directionY = 1;
+            break;
+    }
+});
+
+window.addEventListener('keyup', function(e) {
+    if (e.key === 'ArrowRight' || e.key === 'ArrowLeft') {
+        mosquito.directionX = 0;
+    }
+    if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+        mosquito.directionY = 0;
+    }
+});
 
 
 start()
